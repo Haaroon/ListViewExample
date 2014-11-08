1ebfb50ab5f79dccd32bc3c95399fb2093c77758
Haaroon Y <h.yousaf@hotmail.com>
2014-11-08 14:37:07 +0000

    listview with custom row layout @12.11

--- app/src/main/java/com/example/haaroon/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/haaroon/listviewexample/MainActivity.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,8 +18,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String[] favouriteTVShows = {
+                "Pokemon", "Digimon", "Suits", "Dark Knight",
+                "Gotham", "Superman", "Life of Mars",
+                "Arrow", "Breaking Bad", "Orphan Black"
+        };
+
+        //generates rtows for listview
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, R.layout.row_layotu,
+                R.id.textview1, favouriteTVShows);
+        //create list view
+        ListView theListView = (ListView) findViewById(R.id.MainListView);
+        //tell list view what values it will use
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                    String tvShowPicked = "You Selected " +
+                            String.valueOf( adapterView.getItemAtPosition(position));
+
+                    Toast.makeText( MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT ).show();
+                };
+        });
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
